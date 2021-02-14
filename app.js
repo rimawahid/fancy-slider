96dfaab9950ae4a6b5edc6cdd595d5ee3cc5281b
Rima Akter <rimawahid3@gmail.com>
2021-02-14 16:40:18 +0600

    80% done

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
+const searchInput = document.getElementById('search')
+
+//search to hit Enter key
+
+searchInput.addEventListener("keypress", function(event) {
+    if (event.key == 'Enter'){
+      searchBtn.click();
+    }    
+});
+
 // selected image 
 let sliders = [];
 
@@ -24,27 +34,29 @@ const showImages = (images) => {
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
+    toggleSpinner(false);
   })
-
+   
 }
 
 const getImages = (query) => {
+  toggleSpinner(true);
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => showImages(data.hits))
     .catch(err => console.log(err))
 }
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
+  element.classList.toggle('added');
  
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
   } else {
-    alert('Hey, Already added !')
+    sliders.splice(item, 1);
   }
 }
 var timer
@@ -67,7 +79,13 @@ const createSlider = () => {
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000;
+  let duration = document.getElementById('duration').value || 1000;
+  if(duration >= 1000){
+    duration = duration;
+  }else{
+    alert('Duration cannot be negative or less then 1 second');
+    duration = 1000;
+  }
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
@@ -113,10 +131,21 @@ searchBtn.addEventListener('click', function () {
   document.querySelector('.main').style.display = 'none';
   clearInterval(timer);
   const search = document.getElementById('search');
-  getImages(search.value)
+  getImages(search.value);
   sliders.length = 0;
-})
+});
 
 sliderBtn.addEventListener('click', function () {
   createSlider()
-})
+});
+
+//spinner
+
+const toggleSpinner =(show) =>{
+  const spinner = document.getElementById('loading-spinner');
+  if(show){
+    spinner.classList.remove('d-none');
+  }else{
+    spinner.classList.add('d-none');
+}
+};
